adccdc8aae52f1f01a518e836579ec1f17680d42
samfreitasxs <samuel.sfdf@gmail.com>
2023-06-22 20:46:20 -0300

    feat: implementando spec API

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,10 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    env: {
+      API_URL: 'https://api.coursify.me/v1',
     },
   },
-});
+  fixturesFolder: false,
+})
