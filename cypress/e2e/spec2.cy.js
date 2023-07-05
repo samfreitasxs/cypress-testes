4ae8c766ba8c80d14de069efa6d690f7858b0022
samfreitasxs <samuel.sfdf@gmail.com>
2023-07-05 17:54:11 -0300

    feat: Implementando novos testes

--- cypress/e2e/spec2.cy.js
+++ cypress/e2e/spec2.cy.js
@@ -1,31 +1,28 @@
-/// <reference types="cypress" />
-
-describe ('Testing the homepage of the Testing with Cypress', () => {
-
-    beforeEach(() => {
-        cy.visit('https://commitquality.com/')
+describe('Testes da API da Binance', () => {
+    // Variáveis para armazenar a chave de API e o segredo
+    const apiKey = 'sua_chave_de_api';
+    const apiSecret = 'seu_segredo';
+  
+    it('Deve obter o saldo da conta do usuário', () => {
+      // Requisição para obter o saldo da conta do usuário
+      cy.request({
+        method: 'GET',
+        url: 'https://api.binance.com/api/v3/account',
+        headers: {
+          'X-MBX-APIKEY': apiKey,
+        },
+        qs: {
+          timestamp: Date.now(),
+        },
+        auth: {
+          bearer: apiSecret,
+        },
+      }).then((response) => {
+        // Verifique se a requisição foi bem-sucedida e se o saldo da conta é retornado corretamente
+        expect(response.status).to.equal(200);
+        expect(response.body).to.have.property('balances');
+        expect(response.body.balances).to.be.an('array');
+      });
     });
-
-    it('Verify Add Product', () => {
-        cy
-          .get('[data-testid="navbar-addproduct"]')
-          .should('be.visible')
-          .and('contains.text', 'Add Product')
-          .click()
-    })
-    it('Verify Practice', () => {
-        cy
-          .get('[data-testid="navbar-practice"]')
-          .should('be.visible')
-          .and('contains.text', 'Practice')
-          .click()
-    })
-    it('Verify Practice', () => {
-        cy
-          .get('[data-testid="navbar-practice"]')
-          .should('be.visible')
-          .and('contains.text', 'Practice')
-          .click()
-    })
-
-})
+  });
+  
