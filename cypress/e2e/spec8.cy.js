89521ce7384442e3bcb7cf8f8c6f3565bc4ef4a8
samfreitasxs <samuel.sfdf@gmail.com>
2024-02-05 09:10:22 -0300

    feat: inclusao de novos cns

--- cypress/e2e/spec8.cy.js
+++ cypress/e2e/spec8.cy.js
@@ -35,7 +35,22 @@ describe('Testes das APIs FakeRestAPI v1', () => {
         })
     })
   
-    
+    it('Deve obter a lista de usuários', () => {
+      cy.request('GET', 'http://fakerestapi.azurewebsites.net/api/v1/Users')
+        .then((response) => {
+          expect(response.status).to.eq(200)
+          expect(response.body).to.be.an('array')
+          expect(response.body.length).to.be.greaterThan(0)
+        })
+    })
+    it('Deve obter a lista de usuários', () => {
+      cy.request('GET', 'http://fakerestapi.azurewebsites.net/api/v1/Users')
+        .then((response) => {
+          expect(response.status).to.eq(200)
+          expect(response.body).to.be.an('array')
+          expect(response.body.length).to.be.greaterThan(0)
+        })
+    })
     it('Deve obter a lista de usuários', () => {
       cy.request('GET', 'http://fakerestapi.azurewebsites.net/api/v1/Users')
         .then((response) => {
