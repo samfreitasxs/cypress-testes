d25f09e6eca5923e4dc0dd32733f0d15931717e8
samfreitasxs <samuel.sfdf@gmail.com>
2024-02-21 09:18:40 -0300

    feat: Implementacao spec9

---
+++ cypress/e2e/spec9.cy.js
@@ -0,0 +1,20 @@
+describe('Testes da API de Moedas', () => {
+    it('Deve verificar se a moeda Bitcoin está presente', () => {
+      cy.request('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json')
+        .its('body')
+        .should('include', { "btc": "Bitcoin" }) // Verifica se a moeda Bitcoin está presente
+    })
+  
+    it('Deve verificar se a moeda Ethereum está presente', () => {
+      cy.request('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json')
+        .its('body')
+        .should('include', { "eth": "Ethereum" }) // Verifica se a moeda Ethereum está presente
+    })
+  
+    it('Deve verificar se a moeda Cardano está presente', () => {
+      cy.request('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json')
+        .its('body')
+        .should('include', { "ada": "Cardano" }) // Verifica se a moeda Cardano está presente
+    })
+  })
+  
