82200c41e72d9482a657bd330ce254cc4afbbc37
samfreitasxs <samuel.sfdf@gmail.com>
2024-03-18 11:36:33 -0300

    feat:subindo new

---
+++ cypress/e2e/spec7.cy.js
@@ -0,0 +1,38 @@
+describe('Testes da API API v4', () => {
+    it('Deve obter a lista de atividades', () => {
+      cy.request('GET', 'http://fakerestapi.azurewebsites.net/api/v1/Activities')
+        .then((response) => {
+          expect(response.status).to.eq(200)
+          expect(response.body).to.be.an('array')
+          expect(response.body.length).to.be.greaterThan(0)
+        })
+    })
+  
+    it('Deve obter a lista de livros', () => {
+      cy.request('GET', 'http://fakerestapi.azurewebsites.net/api/v1/Books')
+        .then((response) => {
+          expect(response.status).to.eq(200)
+          expect(response.body).to.be.an('array')
+          expect(response.body.length).to.be.greaterThan(0)
+        })
+    })
+  
+    it('Deve obter a lista de capas de livros', () => {
+      cy.request('GET', 'http://fakerestapi.azurewebsites.net/api/v1/CoverPhotos')
+        .then((response) => {
+          expect(response.status).to.eq(200)
+          expect(response.body).to.be.an('array')
+          expect(response.body.length).to.be.greaterThan(0)
+        })
+    })
+  
+    it('Deve obter a lista de usuários', () => {
+      cy.request('GET', 'http://fakerestapi.azurewebsites.net/api/v1/Users')
+        .then((response) => {
+          expect(response.status).to.eq(200)
+          expect(response.body).to.be.an('array')
+          expect(response.body.length).to.be.greaterThan(0)
+        })
+    })
+  })
+  
